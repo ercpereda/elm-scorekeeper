3006cf3666274679ca74fc52c3ca0d55b62480a7
E. R. C. Pereda <ercp42@gmail.com>
2016-12-10 01:12:21 -0300

    create the model

--- Main.elm
+++ Main.elm
@@ -2,6 +2,38 @@ module Main exposing (..)
 
 import Html exposing (..)
 
+-- model
+
+type alias Model =
+  { palyers: List Player,
+  , name: String
+  , playerId: Maybe Int
+  , plays: List Play
+  }
+
+type alias Player =
+  { id: Int
+  , name: String
+  , points: Int
+  }
+
+type alias Play =
+  { id: Int
+  , playerId: Int
+  , name: String
+  , points: Int
+  }
+
+initModel : Model
+initModel =
+  { players = []
+  , name = ""
+  , playerId = Nothing
+  , plays = []
+  }
+
+
+
 main : Html msg
 main = 
   text "Hello World!"
